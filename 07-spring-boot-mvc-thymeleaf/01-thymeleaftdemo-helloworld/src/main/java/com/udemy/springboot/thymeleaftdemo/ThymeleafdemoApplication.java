b9b9dfe5d5b1ee4fc6112f24af37c13141f3fb81
Gayle Wilson <wilsongaylem@gmail.com>
2026-07-08 12:11:49 -0600

    Hard coded radio inputs

--- 07-spring-boot-mvc-thymeleaf/01-thymeleaftdemo-helloworld/src/main/java/com/udemy/springboot/thymeleaftdemo/ThymeleafdemoApplication.java
+++ 07-spring-boot-mvc-thymeleaf/01-thymeleaftdemo-helloworld/src/main/java/com/udemy/springboot/thymeleaftdemo/ThymeleafdemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThymeleaftdemoApplication {
+public class ThymeleafdemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ThymeleaftdemoApplication.class, args);
+		SpringApplication.run(ThymeleafdemoApplication.class, args);
 	}
 
 }
--- 07-spring-boot-mvc-thymeleaf/01-thymeleaftdemo-helloworld/src/main/java/com/udemy/springboot/thymeleaftdemo/controller/StudentController.java
+++ 07-spring-boot-mvc-thymeleaf/01-thymeleaftdemo-helloworld/src/main/java/com/udemy/springboot/thymeleaftdemo/controller/StudentController.java
@@ -1,18 +1,25 @@
 package com.udemy.springboot.thymeleaftdemo.controller;
 
 import com.udemy.springboot.thymeleaftdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model model) {
         model.addAttribute("student", new Student());
+        model.addAttribute("countries", countries);
         return "student-form";
     }
 
